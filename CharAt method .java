88557412085d1db5e578da1407155c1108d60181
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-24 19:09:37 +0530

    Create CharAt method .java

---
+++ CharAt method .java
@@ -0,0 +1,21 @@
+public class String
+{
+	public static void main(String[] args) 
+	{
+		
+		String a= "hello";
+		String b="there";
+		System.out.println(b.length());
+		System.out.println(a.length());
+		if(a.equals("hello"))
+		{
+		    System.out.println("Print there");
+		    
+		}
+		
+		 System.out.println(a.charAt(3));
+		
+		
+		
+	}
+}
