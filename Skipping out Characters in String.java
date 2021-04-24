578a4341f74a668610f0530ee24252414e372ac7
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-24 18:53:34 +0530

    Create Skipping out Characters in String.java

---
+++ Skipping out Characters in String.java
@@ -0,0 +1,12 @@
+public class String
+{
+	public static void main(String[] args) {
+		
+		String Str = "ABCDEFGH";
+		System.out.println(Str.length());
+		String extractedString = Str.substring(1);
+		System.out.println(extractedString);
+		
+		
+	}
+}
