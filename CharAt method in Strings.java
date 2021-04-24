2c278a9dba1d253663526064047bf85d03ef5162
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-24 19:02:24 +0530

    Create CharAt method in Strings.java

---
+++ CharAt method in Strings.java
@@ -0,0 +1,19 @@
+public class String
+{
+	public static void main(String[] args) 
+	{
+		
+		String a= "hello";
+		String b="there";
+		System.out.println(b.length());
+		System.out.println(a.length());
+		if(a.equals("hello"))
+		{
+		    System.out.println("Print there");
+		    
+		}
+		
+		
+		
+	}
+}
