b6dc8866b7bd1da7d92b7ebe35f5b76f82179e8b
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-28 10:10:13 +0530

    Create Indexof_method.java

---
+++ Indexof_method.java
@@ -0,0 +1,13 @@
+public class String
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		    String str = "Hello Pavan Kalapala";
+		
+		int a = str.indexOf("World");
+		System.out.println(a);
+	}
+		
+	
+}
